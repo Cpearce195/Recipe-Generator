6fdcf37ac055bfda8de6a8074388a929c7bc0cfc
Cpearce195 <165181177+Cpearce195@users.noreply.github.com>
2024-04-05 20:11:12 -0300

    Updated db_test.py

--- db_test.py
+++ db_test.py
@@ -4,7 +4,19 @@ connection = sqlite3.connect('database.db')
 
 cur = connection.cursor()
 
-print(cur.execute("SELECT * FROM Prompts").fetchall())
+id = cur.execute("SELECT ID FROM Prompts").fetchall()
+created = cur.execute("SELECT Created FROM Prompts").fetchall()
+ingredients = cur.execute("SELECT IngredientList FROM Prompts").fetchall()
+msg = cur.execute("SELECT msg FROM Prompts").fetchall()
+
+i = 0;
+
+while i < len(id):
+    print(i)
+    print("ID: " + str(id[i]) + ", Time: " + str(created[i]) + ", Ingredients: " + str(ingredients[i]))
+    print("Recipe: " + str(msg[i]))
+
+    i += 1
 
 connection.commit()
 connection.close()
